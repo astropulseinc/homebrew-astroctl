40c5915ed38c82599ccfab6fe7a9ed8c2e16f07f
Rajesh RC <218767+xnulinu@users.noreply.github.com>
2026-08-01 06:00:16 +0000

    Update astroctl to v0.0.146

--- astroctl.rb
+++ astroctl.rb
@@ -2,8 +2,8 @@ class Astroctl < Formula
   desc "Command-line interface for Astropulse"
   homepage "https://github.com/astropulseinc/astroctl"
   url "https://storage.googleapis.com/astroctl-cli/astroctl-darwin.amd64.tar.gz"
-  version "0.0.145"
-  sha256 "356906dfba311ca5be103fd35057ac2178eb1195eb12d6f0c3511da54bcb559d"
+  version "0.0.146"
+  sha256 "65daa8eb697dee431175c9570757e3e69fd8cab2e9dde9e2a8833d45ee7bc7dd"
   license "MIT"
 
   livecheck do
@@ -13,7 +13,7 @@ class Astroctl < Formula
 
   on_arm do
     url "https://storage.googleapis.com/astroctl-cli/astroctl-darwin.arm64.tar.gz"
-    sha256 "e46033e1a12bf19c42e16366ffa6c5c2af0bf39aff4038cf67cdf351ffcc8e2d"
+    sha256 "cc05dfda283569ae9378eee5af3c3fe589a7023959583dbaea2f7358d1eedc0c"
   end
 
   def install
